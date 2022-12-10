5af7990521e34c5ea1b689774e7c189d08859667
mehoww <michal.gruba00@gmail.com>
2022-12-10 18:32:23 +0100

    Final model

--- model.py
+++ model.py
@@ -10,22 +10,20 @@ def get_model(x):
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(.4))
 
-    model.add(Conv2D(128, (3, 3), activation='relu', use_bias=True, kernel_regularizer=regularizers.l2(l=0.01)))
+    model.add(Conv2D(128, (3, 3), activation='relu'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(.4))
 
     model.add(Flatten())
-    model.add(Dense(64, activation='relu', use_bias=True, kernel_regularizer=regularizers.l2(l=0.01)))
+    model.add(Dense(64, activation='relu'))
     model.add(Dense(256, activation='relu'))
-    # model.add(Dense(512, activation='relu'))
-    # model.add(Dense(256, activation='relu'))
-    model.add(Dense(128, activation='relu', use_bias=True, kernel_regularizer=regularizers.l2(l=0.01)))
+    model.add(Dense(128, activation='relu'))
     model.add(Dense(64, activation='relu'))
     model.add(Dense(32, activation='relu'))
 
     model.add(Dense(3, activation='softmax'))
 
-    optimizer = optimizers.Adam(learning_rate=0.0001)
+    optimizer = optimizers.Adam(learning_rate=0.00001)
     model.compile(loss='sparse_categorical_crossentropy', optimizer=optimizer, metrics=['accuracy'])
 
     return model
